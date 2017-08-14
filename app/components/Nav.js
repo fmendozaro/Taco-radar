47c2a1bf8345a71cda288ae2f361a8283347e27a
Fernando <fernandoza@gmail.com>
2017-08-14 10:30:18 -0500

    Add assets and CSS changes.
    - Add the images for the main page.
    - Created CSS classes for the main colors.
    - Get rid of the materialize template.

--- app/components/Nav.js
+++ app/components/Nav.js
@@ -9,7 +9,7 @@ function Nav(){
                      style={{ backgroundImage: 'url(//scdn.shopify.com/s/files/1/1775/8583/t/1/assets/icon-seamless.png?3602604866228935180)'
                 }}></div>
             </div>
-            <div className="nav-wrapper db">
+            <div className="nav-wrapper bg-green-flag">
                 <a href="/pages/demo" className="brand-logo"><i className="material-icons">face</i>Logo</a>
                 <a href="#" data-activates="nav-mobile" className="button-collapse"><i className="material-icons">menu</i></a>
                 <ul className="bt hide-on-med-and-down">
@@ -30,15 +30,28 @@ function Nav(){
                     </li>
                     <li>
                         <NavLink exact activeClassName="active" to="/" >
+                            Best Pozole places
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink exact activeClassName="active" to="/" >
                             Contact
                         </NavLink>
                     </li>
                 </ul>
 
-                <div className="nav-header de">
-                    <h1>Taco Radar</h1>
-                    <h3>It's not Tex-Mex it's Mex-Mex</h3>
-                    <p className="ge">Welcome to the Taco Radar a place where you can find the nearest and finest real mexican flavored tacos in San Antonio, Texas. If you ever wonder how a real taco taste like, you can give it a shot and see for yourself.</p>
+                <div className="parallax-container">
+                    <div className="parallax"><img src="images/parallax1.jpg" /></div>
+                </div>
+                <div className="section green darken-4">
+                    <div className="row container">
+                        <h1 className="header">Taco Radar</h1>
+                        <h3>It's not Tex-Mex it's Mex-Mex</h3>
+                        <p>Welcome to the Taco Radar a place where you can find the nearest and finest real mexican flavored tacos in San Antonio, Texas. If you ever wonder how a real taco taste like, you can give it a shot and see for yourself.</p>
+                    </div>
+                </div>
+                <div className="parallax-container">
+                    <div className="parallax"><img src="images/parallax2.jpg"/></div>
                 </div>
             </div>
         </nav>
--- app/index.js
+++ app/index.js
@@ -1,10 +1,11 @@
 require('./index.css');
 require('./utils/pacman.css');
-require('./utils/gallery-materialize.min.opt.css');
 
 var React = require('react');
 var ReactDom = require('react-dom');
 var App = require('./components/App');
+
+$('.parallax').parallax();
 
 ReactDom.render(
     <App />,
