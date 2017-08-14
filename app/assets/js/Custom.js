89f492dc7a9b15026d6f051f779de643ea94094b
Fernando <fernandoza@gmail.com>
2017-08-14 16:29:34 -0500

    Fix of the container and some map structure.
    - Google places structure.

--- app/assets/js/Custom.js
+++ app/assets/js/Custom.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
 
+    console.log("Custom js");
+
     $('.parallax').parallax();
     $('.tap-target').tapTarget('open');
 
@@ -8,31 +10,45 @@ $(document).ready(function(){
     var service;
     var infowindow;
 
-    function initialize() {
-        var pyrmont = new google.maps.LatLng(-33.8665433,151.1956316);
+
+    function initMap() {
+        var pyrmont = {lat: -33.867, lng: 151.195};
 
         map = new google.maps.Map(document.getElementById('map'), {
             center: pyrmont,
             zoom: 15
         });
 
-        var request = {
+        infowindow = new google.maps.InfoWindow();
+        var service = new google.maps.places.PlacesService(map);
+        service.nearbySearch({
             location: pyrmont,
-            radius: '500',
-            type: ['restaurant']
-        };
-
-        service = new google.maps.places.PlacesService(map);
-        service.nearbySearch(request, callback);
+            radius: 500,
+            type: ['store']
+        }, callback);
     }
 
     function callback(results, status) {
-        if (status == google.maps.places.PlacesServiceStatus.OK) {
+        if (status === google.maps.places.PlacesServiceStatus.OK) {
             for (var i = 0; i < results.length; i++) {
-                var place = results[i];
                 createMarker(results[i]);
             }
         }
     }
+
+    function createMarker(place) {
+        var placeLoc = place.geometry.location;
+        var marker = new google.maps.Marker({
+            map: map,
+            position: place.geometry.location
+        });
+
+        google.maps.event.addListener(marker, 'click', function() {
+            infowindow.setContent(place.name);
+            infowindow.open(map, this);
+        });
+    }
+
+    initMap();
 
 });
--- app/components/Map.js
+++ app/components/Map.js
@@ -3,9 +3,26 @@ var React = require("react");
 class Map extends React.Component{
     render(){
         return(
-            <main className="content">
-                Map
-                <div id="map"></div>
+            <main className="content container">
+
+                <div className="row">
+                    <div className="col s8 offset-s2">
+                        <form action="/search">
+                            <div className="input-field col s12">
+                                <i className="material-icons prefix">search</i>
+                                <input id="icon_prefix" type="text" className="validate" />
+                                    <label htmlFor="icon_prefix">Address, Zip Code</label>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col s12 m12">
+                        <label htmlFor="map">Results:</label>
+                        <div id="map"></div>
+                    </div>
+                </div>
+
             </main>
         );
     }
